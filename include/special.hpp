98fea928afe9f5ec32503800f910a356ce044e0b
Hunter Land <hunterland457@gmail.com>
2022-04-26 22:46:25 -0700

    time_point supported down to 1s duration

--- include/special.hpp
+++ include/special.hpp
@@ -5,10 +5,15 @@
 #include <ctime>
 #include <system_error>
 
+//NOTE: TODO: These functions are only accurate to 1 second, anything less will be lost and change observed data
+//NOTE: A clock's epoch can be gotten with `std::chrono::time_point<std::chrono::Clock>{}`
+
 void serialize(serialOut& serial, tm time);
 
 template <typename Clock, typename Duration>
 void serialize(serialOut& serial, std::chrono::time_point<Clock, Duration> point) {
+	constexpr bool isSecondsOrMore = ( Duration(1) >= std::chrono::seconds(1) );
+	static_assert(isSecondsOrMore, "Currently only time_points with 1 second or worse accuracy is supported");
 	//tm struct with UTC zone is desired
 	//tm can be gotten in Unix/UTC from gmtime (or variation)
 	//gmtime needs time_t
@@ -31,6 +36,8 @@ void deserialize(serialIn& serial, tm& time);
 
 template <typename Clock, typename Duration>
 void deserialize(serialIn& serial, std::chrono::time_point<Clock, Duration>& point) {
+	constexpr bool isSecondsOrMore = ( Duration(1) >= std::chrono::seconds(1) );
+	static_assert(isSecondsOrMore, "Currently only time_points with 1 second or worse accuracy is supported");
 	//tm is used at the base, so we must convert from a tm structure into our chrono point
 	//tm is stored in Unix/UTC time, and must be converted to a local time_t
 	//unfortunatley, there is no POSIX inverse of gmtime (or variation), so we must do a bit more work here
@@ -50,5 +57,5 @@ void deserialize(serialIn& serial, std::chrono::time_point<Clock, Duration>& poi
 	std::time_t tt = mktime(&timeUnix);
 	tt -= timezone;
 	auto pointGeneric = Clock::from_time_t(tt);
-	point = pointGeneric;
+	point = std::chrono::time_point_cast<Duration>(pointGeneric);
 }
